bd1dff6f4a92e02fadb09295067834e55777eade
hide-afk <abadplatasmarc2007@gmail.com>
2025-08-19 12:53:07 +0200

    added:variables and arrays base prepared

--- JavaScript/mixedMessages_codecademyProject/mixedMessages.js
+++ JavaScript/mixedMessages_codecademyProject/mixedMessages.js
@@ -1,1 +1,22 @@
-console.log("Test for first commit");
+
+const jokeTypes = ['frontEnd','backEnd','qa'];
+let frontEndJokes = [];
+let backEndJokes = [
+    "Backend devs’ favorite pickup line:\nI’ve got great uptime, baby",
+    "Backend devs are like magicians: \nThe user never knows what’s happening behind the curtain.",
+    ""
+];
+let qaJokes = [
+    "A QA walks into a bar and orders a beer. \nThen he orders 0 beers. \nThen he orders -1 beers. \nThen he orders 999999 beers. \nThe bartender explodes.",
+    "",
+    ""
+];
+
+function chooseTypeOfJoke(){
+    const randomInt = 0;
+    return jokeTypes[Math.floor((Math.random() * 3))]; // return a random type of joke from its array 
+}
+console.log("# Mixed Messages Codecademy project - By Marc Abad, aka hide-afk #");
+console.log("Type of joke randomized: " + chooseTypeOfJoke());
+
+
