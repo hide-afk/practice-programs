e0c1cc2d224649cbf4434eb86be23aaab2f0c518
hide-afk <abadplatasmarc2007@gmail.com>
2025-08-20 12:15:12 +0200

    completed: functionally program

--- JavaScript/mixedMessages_codecademyProject/mixedMessages.js
+++ JavaScript/mixedMessages_codecademyProject/mixedMessages.js
@@ -1,22 +1,46 @@
 
+// defining variables and arrays with the needed content
 const jokeTypes = ['frontEnd','backEnd','qa'];
-let frontEndJokes = [];
+let frontEndJokes = [
+    "How do frontend developers like their coffee? Responsive",
+    "Why did the CSS file go to the party alone? Because it had no class",
+    "Why did the frontend developer bring a ladder to work? To deal with z-index problems"
+];
 let backEndJokes = [
-    "Backend devs’ favorite pickup line:\nI’ve got great uptime, baby",
-    "Backend devs are like magicians: \nThe user never knows what’s happening behind the curtain.",
-    ""
+    "Backend devs’ favorite pickup line: I’ve got great uptime, baby",
+    "Backend devs are like magicians:  The user never knows what’s happening behind the curtain.",
+    "Why do backend developers hate nature? Too many bugs"
 ];
 let qaJokes = [
     "A QA walks into a bar and orders a beer. \nThen he orders 0 beers. \nThen he orders -1 beers. \nThen he orders 999999 beers. \nThe bartender explodes.",
-    "",
-    ""
+    "Why don’t QA engineers ever get invited to parties? Because they always find the flaws",
+    "QA prayer: “Dear system, give me this day my daily bug”"
 ];
 
+// empty variables to store the randomized options
+let jokeTypeSelected;
+let jokeSelected;
+
+// set a random type of joke
 function chooseTypeOfJoke(){
-    const randomInt = 0;
-    return jokeTypes[Math.floor((Math.random() * 3))]; // return a random type of joke from its array 
+    jokeTypeSelected = (jokeTypes[Math.floor((Math.random() * 3))] + "Jokes");
+    console.log("Type of joke randomized: " + jokeTypeSelected);
 }
+
+// set a random joke based on their type
+function randomJoke(){
+    if (jokeTypeSelected === "backEndJokes"){
+        jokeSelected = backEndJokes[(Math.floor(Math.random() * 3))];
+    } else if (jokeTypeSelected === "frontEndJokes"){
+        jokeSelected = frontEndJokes[(Math.floor(Math.random() * 3))];
+    } else {
+        jokeSelected = qaJokes[(Math.floor(Math.random() * 3))];
+    }
+    console.log("Joke selected:\n" + jokeSelected);
+}
+
 console.log("# Mixed Messages Codecademy project - By Marc Abad, aka hide-afk #");
-console.log("Type of joke randomized: " + chooseTypeOfJoke());
+chooseTypeOfJoke();
+randomJoke();
 
-
+console.log(jokeTypes.length);
